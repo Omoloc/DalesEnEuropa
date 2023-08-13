c551b661a7684bd60205fb4bfc795629451ade3c
Omoloc <139947644+Omoloc@users.noreply.github.com>
2023-08-13 16:03:26 +0200

    Update juego.js
    añadido feijo al juego

--- juego.js
+++ juego.js
@@ -1,72 +1,26 @@
-// Configuración del juego
-var config = {
-  type: Phaser.AUTO,
-  width: 611,
-  height: 349,
-  scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-};
-
-var game = new Phaser.Game(config);
-
-var score = 0;
-var scoreText;
-var greenCircleTimer;
-var clickedCircle = null;
-
-function preload() {
-  // No se requiere la función preload para este ejemplo
-  
-  this.load.image('congreso', 'congreso.png');
-  this.load.image('feijo','feijo.gif');
-}
+// ... (código previo)
 
 function create() {
-  
   this.add.image(0, 0, 'congreso').setOrigin(0);
 
-  var positions = [
-    { x: 100, y: 100 },
-    { x: 200, y: 100 },
-    { x: 300, y: 100 },
-    { x: 400, y: 100 },
-    { x: 500, y: 100 },
-    { x: 100, y: 200 },
-    { x: 200, y: 200 },
-    { x: 300, y: 200 },
-    { x: 400, y: 200 },
-    { x: 500, y: 200 },
-    { x: 100, y: 300 },
-    { x: 200, y: 300 },
-    { x: 300, y: 300 },
-    { x: 400, y: 300 },
-    { x: 500, y: 300 }
-    // Agrega las demás posiciones aquí...
-  ];
+  // Código de creación de posiciones omitido...
 
-  // Tablero de juego
   for (var i = 0; i < positions.length; i++) {
     var position = positions[i];
-    var circle = this.add.circle(position.x, position.y, 30, 0x000000);
-    circle.setInteractive();
+    var feijo = this.add.image(position.x, position.y, 'feijo');  // Cambio aquí
+    feijo.setDisplaySize(60, 60);  // Ajusta el tamaño de la imagen
+    feijo.setInteractive();
 
-    circle.on('pointerdown', function () {
-      if (this.fillColor === 0x00FF00) {
+    feijo.on('pointerdown', function () {
+      if (this.alpha === 1) {
         clickedCircle = this;
-        this.fillColor = 0xFF0000;
+        this.alpha = 0;  // Cambio aquí
         score += 1;
         scoreText.setText('Score: ' + score);
-        // Temporizador para mostrar círculos verdes cada diez segundos
+        // Temporizador para mostrar imágenes de nuevo cada diez segundos
         greenCircleTimer = this.time.addEvent({
           delay: 100,
-          callback: hideRedCircle,
+          callback: hideFeijo,
           callbackScope: this,
           loop: true
         });
@@ -78,51 +32,47 @@ function create() {
   var style = { fontSize: '32px', fill: '#FFFF00' };
   scoreText = this.add.text(16, 16, 'Score: 0', style);
 
-  // Temporizador para mostrar círculos verdes cada diez segundos
+  // Temporizador para mostrar imágenes de nuevo cada diez segundos
   greenCircleTimer = this.time.addEvent({
     delay: 2000,
-    callback: showGreenCircle,
+    callback: showFeijo,
     callbackScope: this,
     loop: true
   });
 }
 
-function hideRedCircle() {
+function hideFeijo() {
   if (clickedCircle) {
-    clickedCircle.fillColor = 0x000000;
+    clickedCircle.alpha = 1;  // Cambio aquí
     clickedCircle = null;
   }
-  // Temporizador para mostrar círculos verdes cada diez segundos
+  // Temporizador para mostrar imágenes de nuevo cada diez segundos
   greenCircleTimer = this.time.addEvent({
     delay: 2000,
-    callback: showGreenCircle,
+    callback: showFeijo,
     callbackScope: this,
     loop: true
   });
-
 }
 
-function showGreenCircle() {
-  // Oculta todos los círculos
-  var circles = this.children.list.filter(function (child) {
-    return child.fillColor === 0x000000 || child.fillColor === 0x00FF00 ;
+function showFeijo() {
+  // Oculta todas las imágenes
+  var feijos = this.children.list.filter(function (child) {
+    return child.alpha === 0 || child.alpha === 1;
   });
 
-  circles.forEach(function (circle) {
-    circle.fillColor = 0x000000;
+  feijos.forEach(function (feijo) {
+    feijo.alpha = 1;  // Cambio aquí
   });
 
-  // Muestra un círculo verde en una posición aleatoria
-  var randomCircle = Phaser.Utils.Array.GetRandom(circles);
-  if (randomCircle) {
-    randomCircle.fillColor = 0x00FF00;
+  // Muestra una imagen "feijo" en una posición aleatoria
+  var randomFeijo = Phaser.Utils.Array.GetRandom(feijos);
+  if (randomFeijo) {
+    randomFeijo.alpha = 0;  // Cambio aquí
     this.time.delayedCall(2000, function () {
-      randomCircle.fillColor = 0x000000;
+      randomFeijo.alpha = 1;  // Cambio aquí
     });
   }
 }
 
-function update() {
-  // Lógica para el contador y el cambio de colores
-  // Se mueve a la función showGreenCircle()
-}
+// ... (código posterior)
