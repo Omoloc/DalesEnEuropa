d4b31e04aa593bfbd4b0cdd09f79289ca36d2864
Omoloc <139947644+Omoloc@users.noreply.github.com>
2023-08-15 09:50:44 +0200

    pantalla inicial
    añadida pantalla inicial

--- juego.js
+++ juego.js
@@ -23,6 +23,7 @@ var imagesToDisplay = ['feijo', 'abascal', 'diaz', 'sanchez'];
 var gameStarted = false; // Bandera para saber si el juego ha comenzado
 var countdown = 30; // Tiempo de juego en segundos
 var countdownText;
+var startButton;
 
 function preload() {
   this.load.image('congreso', 'congreso.png');
@@ -52,42 +53,8 @@ function create() {
   });
   countdownText.setOrigin(1, 0);
   countdownText.setScrollFactor(0);
-
-  var positions = [
-    { x: 100, y: 100 },
-    { x: 200, y: 100 },
-    { x: 300, y: 100 },
-    { x: 400, y: 100 },
-    { x: 500, y: 100 },
-    { x: 100, y: 200 },
-    { x: 200, y: 200 },
-    { x: 300, y: 200 },
-    { x: 400, y: 200 },
-    { x: 500, y: 200 },
-    { x: 100, y: 300 },
-    { x: 200, y: 300 },
-    { x: 300, y: 300 },
-    { x: 400, y: 300 },
-    { x: 500, y: 300 }
-    // ... (agrega las demás posiciones aquí)
-  ];
-
-  for (var i = 0; i < positions.length; i++) {
-    var position = positions[i];
-    var positionImage = this.add.image(position.x, position.y, Phaser.Math.RND.pick(imagesToDisplay));
-    positionImage.setDisplaySize(80, 80);
-    positionImage.setInteractive(); // Habilita interacción con la imagen
-
-    positionImage.on('pointerdown', function () {
-      if (this.alpha === 1) {
-        score += 1;
-        scoreText.setText('Score: ' + score);
-        this.alpha = 0;
-      }
-    });
-
-    positionImages.push(positionImage);
-  }
+  
+  positionImages = createPositionImages.call(this);
 
  var style = {
     fontSize: '32px',
@@ -110,13 +77,51 @@ scoreText = this.add.text(16, 16, 'Score: 0', style);
   });
 }
 
+function createPositionImages() {
+  var images = [];
+
+  var positions = [
+    { x: 100, y: 100 },
+    { x: 200, y: 100 },
+    { x: 300, y: 100 },
+    { x: 400, y: 100 },
+    { x: 500, y: 100 },
+    { x: 100, y: 200 },
+    { x: 200, y: 200 },
+    { x: 300, y: 200 },
+    { x: 400, y: 200 },
+    { x: 500, y: 200 },
+    { x: 100, y: 300 },
+    { x: 200, y: 300 },
+    { x: 300, y: 300 },
+    { x: 400, y: 300 },
+    { x: 500, y: 300 }
+    // ... (agrega las demás posiciones aquí)
+  ];
+
+  for (var i = 0; i < positions.length; i++) {
+    var position = positions[i];
+    var positionImage = this.add.image(position.x, position.y, '').setOrigin(0.5);
+    positionImage.setDisplaySize(60, 60);
+    positionImage.setInteractive();
+    positionImage.on('pointerdown', increaseScore);
+    images.push(positionImage);
+  }
+
+  return images;
+}
+
+function increaseScore() {
+  if (gameStarted) {
+    score += 1;
+    scoreText.setText('Score: ' + score);
+  }
+}
+
 function startGame() {
   if (!gameStarted) {
     gameStarted = true;
-    // Oculta el botón y comienza el juego
-    this.children.list.forEach(function (child) {
-      child.visible = false;
-    });
+    startButton.visible = false;
 
     greenCircleTimer = this.time.addEvent({
       delay: 1000,
@@ -125,7 +130,6 @@ function startGame() {
       loop: true
     });
 
-    // Temporizador para el tiempo de juego
     this.time.addEvent({
       delay: 1000,
       repeat: countdown,
@@ -133,9 +137,10 @@ function startGame() {
         countdown--;
         countdownText.setText('Tiempo: ' + countdown);
         if (countdown === 0) {
-          endGame();
+          endGame.call(this);
         }
-      }
+      },
+      callbackScope: this
     });
   }
 }
@@ -153,20 +158,24 @@ function endGame() {
     fill: '#FFFFFF'
   }).setOrigin(0.5);
 
-  var restartButton = this.add.text(config.width / 2, config.height / 2 + 50, 'Volver a jugar', {
-    fontSize: '24px',
-    fill: '#FFFFFF'
-  }).setOrigin(0.5);
-  restartButton.setInteractive();
-  restartButton.on('pointerdown', function () {
-    // Reiniciar el juego
-    resultText.destroy();
-    restartButton.destroy();
-    startButton.visible = true;
-    score = 0;
-    scoreText.setText('Score: 0');
+  startButton.visible = true;
+  startButton.setText('Volver a jugar');
+  startButton.setInteractive();
+  startButton.on('pointerdown', restartGame);
+}
+
+function restartGame() {
+  score = 0;
+  scoreText.setText('Score: 0');
+  countdown = 30;
+  countdownText.setText('Tiempo: ' + countdown);
+  startButton.setText('Jugar');
+  startButton.off('pointerdown');
+  positionImages.forEach(function (positionImage) {
+    positionImage.visible = true;
   });
 }
+
 function showNextImage() {
   positionImages.forEach(function (positionImage) {
     positionImage.setAlpha(0);
@@ -186,206 +195,3 @@ function update() {
 
 
 
-
-/* *******
-
-// Configuración del juego
-var config = {
-  type: Phaser.AUTO,
-  width: 611,
-  height: 349,
-  scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-};
-
-var game = new Phaser.Game(config);
-
-var score = 0;
-var scoreText;
-var greenCircleTimer;
-var clickedCircle = null;
-
-function preload() {
-  // No se requiere la función preload para este ejemplo
-  
-  this.load.image('congreso', 'congreso.png');
-  this.load.image('feijo','feijo.gif');
-}
-
-function create() {
-  
-  this.add.image(0, 0, 'congreso').setOrigin(0);
-
-  var positions = [
-    { x: 100, y: 100 },
-    { x: 200, y: 100 },
-    { x: 300, y: 100 },
-    { x: 400, y: 100 },
-    { x: 500, y: 100 },
-    { x: 100, y: 200 },
-    { x: 200, y: 200 },
-    { x: 300, y: 200 },
-    { x: 400, y: 200 },
-    { x: 500, y: 200 },
-    { x: 100, y: 300 },
-    { x: 200, y: 300 },
-    { x: 300, y: 300 },
-    { x: 400, y: 300 },
-    { x: 500, y: 300 }
-    // Agrega las demás posiciones aquí...
-  ];
-
-  for (var i = 0; i < positions.length; i++) {
-    var position = positions[i];
-    var feijo = this.add.image(position.x, position.y, 'feijo');
-    feijo.setDisplaySize(60, 60);
-    feijo.setInteractive();
-
-    feijo.on('pointerdown', function () {
-      if (this.alpha === 1) {
-        clickedCircle = this;
-        this.alpha = 0;
-        score += 1;
-        scoreText.setText('Score: ' + score);
-        greenCircleTimer = this.time.addEvent({
-          delay: 100,
-          callback: hideFeijo,
-          callbackScope: this,
-          loop: true
-        });
-      }
-    });
-  }
-
-  var style = { fontSize: '32px', fill: '#FFFF00' };
-  scoreText = this.add.text(16, 16, 'Score: 0', style);
-
-  greenCircleTimer = this.time.addEvent({
-    delay: 2000,
-    callback: showFeijo,
-    callbackScope: this,
-    loop: true
-  });
-}
-
-function hideFeijo() {
-  if (clickedCircle) {
-    clickedCircle.alpha = 1;
-    clickedCircle = null;
-  }
-
-  greenCircleTimer = this.time.addEvent({
-    delay: 2000,
-    callback: showFeijo,
-    callbackScope: this,
-    loop: true
-  });
-}
-
-function showFeijo() {
-  var feijos = this.children.list.filter(function (child) {
-    return child.alpha === 0 || child.alpha === 1;
-  });
-
-  feijos.forEach(function (feijo) {
-    feijo.alpha = 1;
-  });
-
-  var randomFeijo = Phaser.Utils.Array.GetRandom(feijos);
-  if (randomFeijo) {
-    randomFeijo.alpha = 0;
-    this.time.delayedCall(2000, function () {
-      randomFeijo.alpha = 1;
-    });
-  }
-}
-
-function update() {
-  // Lógica para el contador y el cambio de colores
-  // Se mueve a la función showFeijo()
-}
-
-*********************
-  // Tablero de juego
-  for (var i = 0; i < positions.length; i++) {
-    var position = positions[i];
-    var circle = this.add.circle(position.x, position.y, 30, 0x000000);
-    circle.setInteractive();
-
-    circle.on('pointerdown', function () {
-      if (this.fillColor === 0x00FF00) {
-        clickedCircle = this;
-        this.fillColor = 0xFF0000;
-        score += 1;
-        scoreText.setText('Score: ' + score);
-        // Temporizador para mostrar círculos verdes cada diez segundos
-        greenCircleTimer = this.time.addEvent({
-          delay: 100,
-          callback: hideRedCircle,
-          callbackScope: this,
-          loop: true
-        });
-      }
-    });
-  }
-
-  // Contador
-  var style = { fontSize: '32px', fill: '#FFFF00' };
-  scoreText = this.add.text(16, 16, 'Score: 0', style);
-
-  // Temporizador para mostrar círculos verdes cada diez segundos
-  greenCircleTimer = this.time.addEvent({
-    delay: 2000,
-    callback: showGreenCircle,
-    callbackScope: this,
-    loop: true
-  });
-}
-
-function hideRedCircle() {
-  if (clickedCircle) {
-    clickedCircle.fillColor = 0x000000;
-    clickedCircle = null;
-  }
-  // Temporizador para mostrar círculos verdes cada diez segundos
-  greenCircleTimer = this.time.addEvent({
-    delay: 2000,
-    callback: showGreenCircle,
-    callbackScope: this,
-    loop: true
-  });
-
-}
-
-function showGreenCircle() {
-  // Oculta todos los círculos
-  var circles = this.children.list.filter(function (child) {
-    return child.fillColor === 0x000000 || child.fillColor === 0x00FF00 ;
-  });
-
-  circles.forEach(function (circle) {
-    circle.fillColor = 0x000000;
-  });
-
-  // Muestra un círculo verde en una posición aleatoria
-  var randomCircle = Phaser.Utils.Array.GetRandom(circles);
-  if (randomCircle) {
-    randomCircle.fillColor = 0x00FF00;
-    this.time.delayedCall(2000, function () {
-      randomCircle.fillColor = 0x000000;
-    });
-  }
-}
-
-function update() {
-  // Lógica para el contador y el cambio de colores
-  // Se mueve a la función showGreenCircle()
-}
-*/
