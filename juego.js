70cb0aa3b62297dca17df55c9f751546778086d8
Omoloc <omoloc@yahoo.es>
2024-03-27 10:01:52 +0100

    pruebea

--- juego.js
+++ juego.js
@@ -1,16 +1,37 @@
 class GameOverScene extends Phaser.Scene {
-  constructor() {
+
+  auxcontador = 0;
+  constructor(contador) {
     super({ key: 'GameOverScene' });
     console.log('Constructor GameOverScene');
+    auxcontador=contador;
+    // Vincula el contexto de 'this' en 'startGame' a la instancia de 'GameOverScene'
+    this.startGame = this.startGame.bind(this);
+
   }
 
   preload() {
     // Carga los recursos necesarios para esta escena
   }
+  startGame() {
+    // Cargo escena del juego
+    console.log('PlayGameScene Scene Called');
+  
+    this.scene.start('PlayGameScene');
+  }
 
   create() {
     // Configura el estado inicial de esta escena
-    this.add.text(0, 0, 'Game Over', { fontSize: '32px', fill: '#FFFF00' });
+    this.add.text(120, 10, '¡Enhorabuena!', { fontSize: '32px', fill: '#FFFF00' });
+    this.add.text(120, 100, '¡Has eliminado a ' + contador + 'del Congreso!', { fontSize: '32px', fill: '#FFFF00' });
+
+    this.startButton = this.add.text(config.width / 2, config.height / 2, 'Jugar de nuevo', {
+      fontSize: '32px',
+      fill: '#FF0000'
+    }).setOrigin(0.5);
+    
+    this.startButton.setInteractive();
+    this.startButton.on('pointerdown', this.startGame);
   }
 
   update() {
@@ -20,181 +41,185 @@ class GameOverScene extends Phaser.Scene {
 
 
 class PlayGameScene extends Phaser.Scene {
-score = 0;
-scoreText;
-positionImages = [];
-imagesToDisplay = ['feijo', 'abascal', 'diaz', 'sanchez'];
-gameStarted = false; // Bandera para saber si el juego ha comenzado
-countdown = 30; // Tiempo de juego en segundos
-countdownText;
+  constructor() {
+    super({ key: 'PlayGameScene' });
+    console.log('Constructor PlayGameScene');
 
-constructor() {
-  super({ key: 'PlayGameScene' });
-  console.log('Constructor PlayGameScene');
-
-}
-
-preload() {
-  this.load.image('congreso', 'congreso.png');
-  this.load.image('feijo', 'feijo.gif');
-  this.load.image('abascal', 'abascal.gif');
-  this.load.image('diaz', 'diaz.gif');
-  this.load.image('sanchez', 'sanchez.gif');
-}
-
-create() {
-  this.add.image(0, 0, 'congreso').setOrigin(0);
-
-  this.scoreText = this.add.text(16, 16, 'Score: 0',
-                            { fontSize: '32px', fill: '#FFFF00' });
-  this.scoreText.setScrollFactor(0);
-
-  this.countdownText = this.add.text(300, 16, 'Tiempo: ' + countdown,
-                                { fontSize: '32px', fill: '#FFFF00' });
-  //countdownText.setOrigin(1, 0);
-  this.countdownText.setScrollFactor(0);
-  
-  this.positionImages = createPositionImages.call(this);
-
-  style = {
-    fontSize: '32px',
-    fill: '#FFFF00',
-    backgroundColor: '#000000',  // Agrega el color de fondo negro al estilo
-    padding: {
-      left: 10,
-      right: 10,
-      top: 5,
-      bottom: 5
-    }
-  };
-  //scoreText = this.add.text(16, 16, 'Score: 0', style);
-  //countdownText = this.add.text(236, 16, 'Tiempo: ' + countdown, style);
-
-  greenCircleTimer = this.time.addEvent({
-    delay: 1000,
-    callback: showNextImage,
-    callbackScope: this,
-    loop: true
-  });
-}
-
-createPositionImages() {
-  images = [];
-
-  positions = [
-    { x: 100, y: 100 },
-    { x: 200, y: 100 },
-    { x: 300, y: 100 },
-    { x: 400, y: 100 },
-    { x: 500, y: 100 },
-    { x: 100, y: 200 },
-    { x: 200, y: 200 },
-    { x: 300, y: 200 },
-    { x: 400, y: 200 },
-    { x: 500, y: 200 },
-    { x: 100, y: 300 },
-    { x: 200, y: 300 },
-    { x: 300, y: 300 },
-    { x: 400, y: 300 },
-    { x: 500, y: 300 }
-    // ... (agrega las demás posiciones aquí)
-  ];
-
-  for (var i = 0; i < positions.length; i++) {
-    position = positions[i];
-    positionImage = this.add.image(position.x, position.y, '').setOrigin(0.5);
-    positionImage.setDisplaySize(60, 60);
-    positionImage.setInteractive();
-    positionImage.on('pointerdown', increaseScore);
-    images.push(positionImage);
+    // Bind the context
+    this.increaseScore = this.increaseScore.bind(this);
+    this.showNextImage = this.showNextImage.bind(this);
+    this.endGame = this.endGame.bind(this);
   }
 
-  return images;
-}
-
-increaseScore() {
-  if (gameStarted) {
-    score += 1;
-    scoreText.setText('Score: ' + score);
+  increaseScore() {
+    console.log('increaseScore called');
+    this.score += 1;
+    this.scoreText.setText('Score: ' + this.score);
   }
-}
+  showNextImage() {
+    this.positionImages.forEach(function (positionImage) {
+      positionImage.setAlpha(0);
+    });
 
-startGame() {
-  if (!gameStarted) {
-    gameStarted = true;
-    startButton.visible = false;
+    this.randomImage = Phaser.Math.RND.pick(this.imagesToDisplay);
+    this.randomPositionImage = Phaser.Math.RND.pick(this.positionImages);
 
-    // Elimina el evento de temporizador anterior
-    if (greenCircleTimer) {
-      greenCircleTimer.remove();
-    }
+    this.randomPositionImage.setTexture(this.randomImage);
+    this.randomPositionImage.setAlpha(1);
+  }
+  endGame() {
+    this.scoreText.visible = false;
+    this.countdownText.visible = false;
+    this.positionImages.forEach(function (positionImage) {
+      positionImage.visible = false;
+    }.bind(this));
 
-    // Crea un nuevo evento de temporizador
-    greenCircleTimer = this.time.addEvent({
+    this.resultText = this.add.text(config.width / 2, config.height / 2 - 50, 'Puntuación: ' + score, {
+      fontSize: '32px',
+      fill: '#FFFFFF'
+    }).setOrigin(0.5);
+
+  }
+
+
+  score = 0;
+  positionImages = [];
+  imagesToDisplay = ['feijo', 'abascal', 'diaz', 'sanchez'];
+  countdown = 30; // Tiempo de juego en segundos
+  scoreText ="";
+  countdownText="";
+
+
+  preload() {
+    console.log('Preload PlayGameScene');
+    this.load.image('congreso', 'congreso.png');
+    this.load.image('feijo', 'feijo.gif');
+    this.load.image('abascal', 'abascal.gif');
+    this.load.image('diaz', 'diaz.gif');
+    this.load.image('sanchez', 'sanchez.gif');
+  }
+
+  create() {
+    console.log('Create PlayGameScene');
+    this.countdown = 30; // Tiempo de juego en segundos
+
+    this.add.image(0, 0, 'congreso').setOrigin(0);
+
+    this.scoreText = this.add.text(16, 16, 'Score: 0',
+                              { fontSize: '32px', fill: '#FFFF00' });
+    this.scoreText.setScrollFactor(0);
+
+    this.countdownText = this.add.text(300, 16, 'Tiempo: ' + this.countdown,
+                                  { fontSize: '32px', fill: '#FFFF00' });
+    //countdownText.setOrigin(1, 0);
+    this.countdownText.setScrollFactor(0);
+    
+    this.positionImages = this.createPositionImages();
+
+    this.style = {
+      fontSize: '32px',
+      fill: '#FFFF00',
+      backgroundColor: '#000000',  // Agrega el color de fondo negro al estilo
+      padding: {
+        left: 10,
+        right: 10,
+        top: 5,
+        bottom: 5
+      }
+    };
+    //scoreText = this.add.text(16, 16, 'Score: 0', style);
+    //countdownText = this.add.text(236, 16, 'Tiempo: ' + countdown, style);
+
+    this.greenCircleTimer = this.time.addEvent({
       delay: 1000,
-      callback: showNextImage,
+      callback: this.showNextImage,
+      callbackScope: this,
+      loop: true
+    });
+    this.showNextImage();
+
+    this.countdownTimer = this.time.addEvent({
+      delay: 1000,
+      callback: this.decreaseCountdown,
       callbackScope: this,
       loop: true
     });
 
-    this.time.addEvent({
-      delay: 1000,
-      repeat: countdown,
-      callback: function () {
-        countdown--;
-        countdownText.setText('Tiempo: ' + countdown);
-        if (countdown === 0) {
-          endGame.call(this);
-        }
-      },
-      callbackScope: this
-    });
   }
+
+  createPositionImages() {
+    this.images = [];
+
+    this.positions = [
+      { x: 100, y: 100 },
+      { x: 200, y: 100 },
+      { x: 300, y: 100 },
+      { x: 400, y: 100 },
+      { x: 500, y: 100 },
+      { x: 100, y: 200 },
+      { x: 200, y: 200 },
+      { x: 300, y: 200 },
+      { x: 400, y: 200 },
+      { x: 500, y: 200 },
+      { x: 100, y: 300 },
+      { x: 200, y: 300 },
+      { x: 300, y: 300 },
+      { x: 400, y: 300 },
+      { x: 500, y: 300 }
+      // ... (agrega las demás posiciones aquí)
+    ];
+
+    for (var i = 0; i < this.positions.length; i++) {
+      this.position = this.positions[i];
+      this.positionImage = this.add.image(this.position.x, this.position.y, '').setOrigin(0.5);
+      this.positionImage.setDisplaySize(30, 30);
+      this.positionImage.setInteractive();
+      this.positionImage.on('pointerdown', this.increaseScore);
+      this.images.push(this.positionImage);
+    }
+
+    return this.images;
+  }
+
+  decreaseCountdown() {
+    console.log('decreaseCountdown called');
+
+    this.countdown--;
+    this.countdownText.setText('Tiempo: ' + this.countdown);
+    if (this.countdown === 0) {
+      this.scene.start('GameOverScene');
+    }
 }
-endGame() {
-  gameStarted = false;
-  scoreText.visible = false;
-  countdownText.visible = false;
-  positionImages.forEach(function (positionImage) {
-    positionImage.visible = false;
-  });
 
-  resultText = this.add.text(config.width / 2, config.height / 2 - 50, 'Puntuación: ' + score, {
-    fontSize: '32px',
-    fill: '#FFFFFF'
-  }).setOrigin(0.5);
-
-  startButton.visible = true;
-  startButton.setText('Volver a jugar');
-  startButton.setInteractive();
-  startButton.on('pointerdown', restartGame);
-}
-
-restartGame() {
-  score = 0;
-  scoreText.setText('Score: 0');
-  countdown = 30;
-  countdownText.setText('Tiempo: ' + countdown);
-  startButton.setText('Jugar');
-  startButton.off('pointerdown');
-  positionImages.forEach(function (positionImage) {
-    positionImage.visible = true;
-  });
-}
-
-showNextImage() {
-  if (gameStarted) {
-    positionImages.forEach(function (positionImage) {
-      positionImage.setAlpha(0);
+  startGame() {
+    // Elimina el evento de temporizador anterior
+    if (this.greenCircleTimer) {
+      this.greenCircleTimer.remove();
+    }
+  
+    // Crea un nuevo evento de temporizador
+    this.greenCircleTimer = this.time.addEvent({
+      delay: 1000,
+      callback: this.showNextImage,
+      callbackScope: this,
+      loop: true
     });
 
-    randomImage = Phaser.Math.RND.pick(imagesToDisplay);
-    randomPositionImage = Phaser.Math.RND.pick(positionImages);
+    if (this.countdownTimer) {
+      this.countdownTimer.remove();
+    }
 
-    randomPositionImage.setTexture(randomImage);
-    randomPositionImage.setAlpha(1);
-   }
-}
+    this.countdownTimer = this.time.addEvent({
+      delay: 1000,
+      callback: this.decreaseCountdown,
+      callbackScope: this,
+      loop: true
+    });
+
+  }
+  
+  
+
 }
 
 
