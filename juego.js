1b04bc9800a68144fe4e70056f0fc5415bd37423
Omoloc <omoloc@yahoo.es>
2024-03-29 18:09:38 +0100

    Añadido Saber más y fondo en la pantalla final

--- juego.js
+++ juego.js
@@ -1,6 +1,17 @@
 class GameOverScene extends Phaser.Scene {
 
   contador = 0;
+  textContent = ""
+  mensajes = 0;
+  moreButton = "";
+
+  style = {
+    fontSize: '32px',
+    fontFamily: 'Arial',
+    fill: '#000000',
+    wordWrap: { width: 580, useAdvancedWrap: true }
+  }
+    
 
   constructor() {
     super({ key: 'GameOverScene' });
@@ -8,6 +19,7 @@ class GameOverScene extends Phaser.Scene {
 
     // Vincula el contexto de 'this' en 'startGame' a la instancia de 'GameOverScene'
     this.startGame = this.startGame.bind(this);
+    this.updateText = this.updateText.bind(this);
   }
 
   init(data) {
@@ -25,20 +37,30 @@ class GameOverScene extends Phaser.Scene {
     console.log('PlayGameScene Scene Called');
     if (this.sound.context.state === 'suspended') {
       this.sound.context.resume();
-  }
+    }
     this.scene.start('PlayGameScene');
   }
-   
+
   create() {
+
+    this.mensajes = 0;
     // Configura el estado inicial de esta escena
     this.add.image(0, 0, 'background').setOrigin(0);
      // Cambia el color de fondo a blanco
     this.cameras.main.setBackgroundColor('#FFFFFF');
   
-    this.add.text(120, 10, '¡Enhorabuena!', { fontSize: '32px', fontFamily: 'Arial', fill: '#000000' });
-    this.add.text(20, 50, '¡Has eliminado a '+this.contador+ ' diputados!', { fontSize: '32px', fontFamily: 'Arial' , fill: '#000000' });
+    //this.add.text(120, 10, '¡Enhorabuena!', { fontSize: '32px', fontFamily: 'Arial', fill: '#000000' });
+    //this.add.text(20, 50, '¡Has eliminado a '+this.contador+ ' diputados!', { fontSize: '32px', fontFamily: 'Arial' , fill: '#000000' });
+    if (this.contador === 0) {
+      this.textContent = this.add.text(20, 16, '¡No has eliminado a ningún diputado!\nNo tengas miedo de golpearles, es solo un juego', this.style );
+    }
+    else
+    {
+      this.textContent = this.add.text(20, 16, '      ¡Enhorabuena!\n¡Has eliminado a '+this.contador+ ' diputados!', this.style );
+    }
 
-    this.startButton = this.add.text(100, 250, '  Jugar\nde nuevo', {
+    // Boton de jugar de nuevo  
+    this.startButton = this.add.text(20, 260, '   Jugar\nde nuevo', {
       fontSize: '32px',
       fontFamily: 'Arial' ,
       fill: '#FFAA00'
@@ -46,13 +68,50 @@ class GameOverScene extends Phaser.Scene {
         this.startButton.setInteractive();
     this.startButton.on('pointerdown', this.startGame);
 
-    this.link = this.add.text(350, 250, 'Saber\nmás', { fontSize: '32px', fontFamily: 'Arial', fill: '#FFAA00'});
+    //botón de saber más
+    this.moreButton = this.add.text(220, 260, 'Saber\n más', {
+      fontSize: '32px',
+      fontFamily: 'Arial' ,
+      fill: '#FFAA00'
+    }).setOrigin(0);
+        this.moreButton.setInteractive();
+    this.moreButton.on('pointerdown', this.updateText);
+
+    //Botón de compartir
+    this.link = this.add.text(420, 260, 'Compartir\nen Twitter', { fontSize: '32px', fontFamily: 'Arial', fill: '#FFAA00'});
     this.link.setInteractive({ useHandCursor: true });  // Hace que el cursor cambie a una mano al pasar por encima
     this.link.on('pointerup', () => {
-        window.open('http://escanos.org', '_blank'); // Abre el enlace en una nueva pestaña
+        window.open('https://twitter.com/intent/tweet?text=Me ha gustado mucho este juego. Te invito a jugarlo y a seguir a @escanosenblanco http://escanos.org', '_blank'); // Abre el enlace en una nueva pestaña
     });
 
   }
+   
+  updateText() {
+    console.log('AboutEB Scene Called');
+    // Cargo escena de saber más
+    this.mensajes += 1;
+    switch (this.mensajes) {
+      case 1:
+        this.textContent.setText('Escaños en Blanco es una alternativa útil al voto en blanco, voto nulo y abstención que permite a los ciudadanos dejar escaños vacíos como forma de rechazo a los candidatos o el sistema');
+        break;
+      case 2:   
+        this.textContent.setText('Escaños en Blanco se presenta a las elecciones con un partido, y no toma posesión de los escaños obtenidos.\nCon ello renuncia al escaño y a cualquier sueldo o subvención que pudiera corresponderle.');
+        break;
+      case 3:   
+        this.textContent.setText('De esa forma de visibiliza a aquellas personas que buscamos cambios profundos en la política y que no nos sentimos representados por los partidos tradicionales.');
+        break;
+      case 4:   
+        this.textContent.setText('Con esta iniciativa se busca:\n1. Visibilizar el descontento\n2. llamar la atención de los medios\n3.Abrir un debate sobre los deficits de nuestro sistema');
+        break;
+      case 5:
+        window.open('https://escanos.org', '_blank'); // Abre el enlace en una nueva pestaña
+        this.moreButton.setText('');
+        this.mensajes = 0;
+        break;
+      }
+  
+  }
+
 
   update() {
     // Actualiza el estado de esta escena en cada frame
@@ -123,7 +182,7 @@ class PlayGameScene extends Phaser.Scene {
   score = 0;
   positionImages = [];
   imagesToDisplay = ['feijo', 'abascal', 'diaz', 'sanchez'];
-  countdown = 30; // Tiempo de juego en segundos
+  countdown = 0; // Tiempo de juego en segundos
   scoreText ="";
   countdownText="";
   soundOpened = null;
@@ -144,7 +203,7 @@ class PlayGameScene extends Phaser.Scene {
 
   create() {
     console.log('Create PlayGameScene');
-    this.countdown = 30; // Tiempo de juego en segundos
+    this.countdown = 2; // Tiempo de juego en segundos
     this.score = 0;
 
     this.add.image(0, 0, 'congreso').setOrigin(0);
@@ -209,7 +268,7 @@ class PlayGameScene extends Phaser.Scene {
       { x: 500, y: 104 },
       { x: 100, y: 195 },
       { x: 200, y: 193 },
-      { x: 300, y: 192 },
+      { x: 300, y: 190 },
       { x: 400, y: 192 },
       { x: 500, y: 192 },
       { x: 100, y: 304 },
@@ -348,7 +407,7 @@ function create() {
 }
 
 function update() {
-  this.add.text(598, 340, '1.3', { fontSize: '8px', fontFamily: 'Arial', fill: '#000000' }) 
+  this.add.text(598, 340, '1.5', { fontSize: '8px', fontFamily: 'Arial', fill: '#FFFFFF' }) 
 }
 
 
